0533acc2ec30142b39720f65a8dcad3150d4bcdb
Abdullah Kiran <abdukiran@gmail.com>
2024-06-14 20:35:55 +0200

    add 24h lock for /random "recipes of today" in android client

--- src/main/java/com/mobilecomputing/backend/service/RecipeService.java
+++ src/main/java/com/mobilecomputing/backend/service/RecipeService.java
@@ -2,9 +2,10 @@ package com.mobilecomputing.backend.service;
 
 import com.mobilecomputing.backend.model.Recipe;
 import com.mobilecomputing.backend.repository.RecipeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -12,7 +13,8 @@ import java.util.Random;
 
 @Service
 public class RecipeService {
-
+    private List<Recipe> recipes = new ArrayList<>();
+    private LocalDateTime lastChangedTime;
     private final RecipeRepository recipeRepository;
 
     public RecipeService(RecipeRepository recipeRepository) {
@@ -20,16 +22,23 @@ public class RecipeService {
     }
 
     public List<Recipe> getRandomRecipes(){
-        List<Recipe> recipes = new ArrayList<>();
-        int lowerBound = 1;
-        int upperBound = (int) recipeRepository.count();
-        Random random = new Random();
+        LocalDateTime currentTime = LocalDateTime.now();
 
-        for (int i = 0; i < 5; i++) {
-            int randomNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
-            Optional<Recipe> optionalRecipe = recipeRepository.findById(randomNumber);
-            optionalRecipe.ifPresent(recipes::add);
+        if(lastChangedTime == null || ChronoUnit.HOURS.between(lastChangedTime, currentTime) >= 24) {
+            int lowerBound = 1;
+            int upperBound = (int) recipeRepository.count();
+            Random random = new Random();
+
+            recipes.clear();
+
+            for (int i = 0; i < 5; i++) {
+                int randomNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+                Optional<Recipe> optionalRecipe = recipeRepository.findById(randomNumber);
+                optionalRecipe.ifPresent(recipes::add);
+            }
         }
+
+        lastChangedTime = currentTime;
 
         return recipes;
     }
